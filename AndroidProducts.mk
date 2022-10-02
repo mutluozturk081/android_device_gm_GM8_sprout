3a4a5de50f2b61c78bc5d62c233a1365aa6892de
mutluemir081 <78675687+mutluemir081@users.noreply.github.com>
2022-10-02 08:51:29 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_GM8_sprout.mk
+    $(LOCAL_DIR)/lineage_GM8_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_GM8_sprout-eng \
-    arrow_GM8_sprout-userdebug \
-    arrow_GM8_sprout-user
+    lineage_GM8_sprout-eng \
+    lineage_GM8_sprout-userdebug \
+    lineage_GM8_sprout-user
