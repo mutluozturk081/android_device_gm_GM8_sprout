276b81628587a3c6c386a414da30dc3b706c1e76
mutluozturk081 <114897370+mutluozturk081@users.noreply.github.com>
2022-10-05 16:47:26 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_GM8_sprout.mk
+    $(LOCAL_DIR)/carbon_GM8_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_GM8_sprout-eng \
-    lineage_GM8_sprout-userdebug \
-    lineage_GM8_sprout-user
+    carbon_GM8_sprout-eng \
+    carbon_GM8_sprout-userdebug \
+    carbon_GM8_sprout-user
